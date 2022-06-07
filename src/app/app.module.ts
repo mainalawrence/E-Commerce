94f9889c9b122a02e24a33364f45d588b69e22a2
maina lawrence <mainalawrence32@gmail.com>
2022-06-07 18:07:44 +0300

    system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsModule } from './productsModule/products.module';
+import { FooterComponent } from './HeaderFooter/footer/footer.component';
+import { HeaderComponent } from './HeaderFooter/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/productsModule/Components/Product-View/product-list/product-list.component.ts
+++ src/app/productsModule/Components/Product-View/product-list/product-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+  products:any[]=[1,2,3,4,5,6,67,7,8,9,9,70,3,2]
   constructor() { }
 
   ngOnInit(): void {
