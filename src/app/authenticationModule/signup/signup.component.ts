6b8c7041badf209acf079ec254f6b1338b37211b
maina lawrence <mainalawrence32@gmail.com>
2022-06-13 15:23:40 +0300

    app

--- src/app/authenticationModule/signup/signup.component.ts
+++ src/app/authenticationModule/signup/signup.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css','../login/login.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+ signinForm!:FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+      this.signinForm= this.fb.group({
+      email:['',[Validators.email,Validators.required]],
+      password:['',[Validators.required]],
+      name:['',[Validators.required]],
+    })
+  }
+  onSubmit(){
+    
   }
 
 }
